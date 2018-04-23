ea831b50627034b97cfcdafcddd53a10dac26547
Woombat84 <33546737+Woombat84@users.noreply.github.com>
2018-04-23 18:24:00 +0200

    Add files via upload

---
+++ masterdriv/masterdriv.ino
@@ -0,0 +1,31 @@
+
+int x;
+#include <Wire.h>
+#include <I2C_Anything.h>
+void setup(){
+  Wire.begin();        
+  Serial.begin(9600);  
+}
+
+void loop() { 
+  Wire.requestFrom(3,4);    
+  while (Wire.available()){ 
+    int temp; 
+    I2C_readAnything(temp);
+    Serial.print(temp);
+    Serial.println("C");
+    int humi;
+    I2C_readAnything(humi);
+    Serial.print(humi);
+    Serial.println("fugt nivaue");
+  }  
+  
+  while(Serial.available()){  
+  x = Serial.parseInt();
+  int junk = Serial.read();
+  Wire.beginTransmission(21); 
+  I2C_writeAnything(x) ;              
+  Wire.endTransmission();    
+  } 
+  delay(100);
+}
